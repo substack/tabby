5857994afa8787e056c82c80fcf562319ab743ee
James Halliday <mail@substack.net>
2013-10-29 18:46:33 -0500

    update the title in the browser with the href

--- example/browser.js
+++ example/browser.js
@@ -1,1 +1,8 @@
 var tabby = require('../')('#content');
+
+var section = document.querySelector('#section');
+
+tabby.on('show', function (href) {
+    var title = href.split('?')[0].split('/').slice(-1)[0];
+    section.textContent = title || 'home';
+});
