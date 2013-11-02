5fc1104632154fd04f1996757616677b1f7f5eec
James Halliday <mail@substack.net>
2013-11-01 18:26:56 -0700

    routing test failing again

--- test/route.js
+++ test/route.js
@@ -2,15 +2,17 @@ var test = require('tape');
 var tabby = require('../');
 
 test(function (t) {
-    t.plan(4);
+    t.plan(5);
     
     var tabs = tabby();
     tabs.add('/beep', { id: 'A' });
     tabs.add('/beep/:msg', { id: 'B' });
     tabs.add('/beep/boop', { id: 'C' });
+    tabs.add('/:name', { id: 'D' });
     
     t.equal(tabs._match('/beep').route.id, 'A');
     t.equal(tabs._match('/beep/xyz').route.id, 'B');
     t.equal(tabs._match('/beep/boop').route.id, 'C');
     t.equal(tabs._match('/beep/x').route.id, 'B');
+    t.equal(tabs._match('/eeee').route.id, 'D');
 });
